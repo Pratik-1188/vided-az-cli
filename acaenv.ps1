690704345437222537d1cc81659aa0bea53b8bf4
Pratik Bhosale <pratikbhosale1188@gmail.com>
2024-08-25 15:58:02 +0530

    added storage mount to env

--- acaenv.ps1
+++ acaenv.ps1
@@ -5,6 +5,19 @@ $CONTAINER_APP_ENV_NAME="vided-container-env"
 az containerapp env create `
   --name $CONTAINER_APP_ENV_NAME `
   --resource-group $RESOURCE_GROUP `
-  --location eastus `
+  --location $LOCATION `
   --enable-workload-profiles `
   --logs-destination none
+
+
+$STORAGE_KEY = $(az storage account keys list --resource-group $RESOURCE_GROUP --account-name $STORAGE_ACCOUNT_NAME --query "[0].value" --output tsv)
+Write-Output $STORAGE_KEY
+
+az containerapp env storage set `
+  --access-mode ReadWrite `
+  --account-name $STORAGE_ACCOUNT_NAME `
+  --azure-file-account-key $STORAGE_KEY `
+  --azure-file-share-name $SHARE_NAME `
+  --name $CONTAINER_APP_ENV_NAME `
+  --resource-group $RESOURCE_GROUP `
+  --storage-name mount
